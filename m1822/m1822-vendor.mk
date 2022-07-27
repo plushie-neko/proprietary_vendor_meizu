5ff1bf39e6257ab9d55c9c41c0efe1653cc255fa
Roman Rihter <teledurak@gmail.com>
2022-07-27 03:23:28 +0300

    m1822: Update camera libraries
    From Flyme 8.0.5.0A, see https://github.com/meizucustoms/proprietary_vendor_meizu/commit/81f7bdbe39d4c6fd6e882d1c359b6bd0275b0559
    for patches description.
    
    Change-Id: I6595a23b6d8d3da9c28bd92adad05abb68db9321

--- m1822/m1822-vendor.mk
+++ m1822/m1822-vendor.mk
@@ -1614,6 +1614,7 @@ PRODUCT_COPY_FILES += \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_qtech_cpp_liveshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_qtech_cpp_liveshot.so \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_qtech_cpp_preview.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_qtech_cpp_preview.so \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_qtech_cpp_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_qtech_cpp_snapshot.so \
+    vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_qtech_cpp_snapshot_night.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_qtech_cpp_snapshot_night.so \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_qtech_cpp_snapshot_remosaic.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_qtech_cpp_snapshot_remosaic.so \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_qtech_cpp_video.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_qtech_cpp_video.so \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_qtech_default_preview_3a.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_qtech_default_preview_3a.so \
@@ -1629,6 +1630,7 @@ PRODUCT_COPY_FILES += \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_stech_cpp_liveshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_stech_cpp_liveshot.so \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_stech_cpp_preview.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_stech_cpp_preview.so \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_stech_cpp_snapshot.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_stech_cpp_snapshot.so \
+    vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_stech_cpp_snapshot_night.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_stech_cpp_snapshot_night.so \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_stech_cpp_snapshot_remosaic.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_stech_cpp_snapshot_remosaic.so \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_stech_cpp_video.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_stech_cpp_video.so \
     vendor/meizu/m1822/proprietary/vendor/lib/libchromatix_s5k3p3sm_stech_default_preview_3a.so:$(TARGET_COPY_OUT_VENDOR)/lib/libchromatix_s5k3p3sm_stech_default_preview_3a.so \
