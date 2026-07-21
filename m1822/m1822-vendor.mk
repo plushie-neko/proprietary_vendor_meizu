6c2c8d924a0b03f25b95251d3e687ac31aad2692
sunflowers.warp@gmail.com <sunflowers.warp@gmail.com>
2026-07-21 13:06:29 +0300

    f

--- m1822/m1822-vendor.mk
+++ m1822/m1822-vendor.mk
@@ -118,7 +118,6 @@ PRODUCT_COPY_FILES += \
     vendor/meizu/m1822/proprietary/system_ext/framework/dpmapi.jar:$(TARGET_COPY_OUT_SYSTEM_EXT)/framework/dpmapi.jar \
     vendor/meizu/m1822/proprietary/system_ext/framework/embmslibrary.jar:$(TARGET_COPY_OUT_SYSTEM_EXT)/framework/embmslibrary.jar \
     vendor/meizu/m1822/proprietary/system_ext/framework/qcrilhook.jar:$(TARGET_COPY_OUT_SYSTEM_EXT)/framework/qcrilhook.jar \
-
     vendor/meizu/m1822/proprietary/system_ext/lib/extractors/libmmparserextractor.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib/extractors/libmmparserextractor.so \
     vendor/meizu/m1822/proprietary/system_ext/lib/libavenhancements.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib/libavenhancements.so \
     vendor/meizu/m1822/proprietary/system_ext/lib/libmmosal.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib/libmmosal.so \
